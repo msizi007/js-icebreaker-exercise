648f5b120e92e37fa6cc6309407d236ee0f39eb7
siyabonga <siyabongakhanyile76@gmail.com>
2025-08-08 11:36:36 +0200

    Add partner 2 variables

--- icebreaker.js
+++ icebreaker.js
@@ -7,3 +7,20 @@ A value representing whether you have seen the movie Inception.
 let favouriteMovie = "Star Wars";
 let numberOfSiblings = 3;
 let hasSeenInception = true;
+
+// A description of a dream vacation spot, including the country, city, and a planned activity.
+// A list of your top 3 favorite foods.
+// A variable for a user's discount code, which is intentionally left empty because they don't have one.
+
+const dreamVacationSpot = {
+    country : "France",
+    city : "Paris",
+    activity : "Put a lock on the Eiffel tower"
+}
+
+const favouriteFoods = ["briyana","samp","chakalaka"]
+
+const userDiscountCode = null
+
+console.log(`My vacation is going to be at ${dreamVacationSpot.country},${dreamVacationSpot.city} the activity will be '${dreamVacationSpot.activity}' and my favourite foods are ${favouriteFoods}.The user discount code is ${userDiscountCode}`)
+
