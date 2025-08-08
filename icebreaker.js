982605545f07cdadbf71b89e2a970fded2c06c41
msizi007 <msizimwelase007@gmail.com>
2025-08-08 11:24:46 +0200

    has added scenario 1-3

---
+++ icebreaker.js
@@ -0,0 +1,9 @@
+/*
+The name of your favorite movie.
+The number of siblings you have.
+A value representing whether you have seen the movie Inception.
+*/
+
+let favouriteMovie = "Star Wars";
+let numberOfSiblings = 3;
+let hasSeenInception = true;
